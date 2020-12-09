80a2477a6308b8372c09a48f50e9f2272c5be47a
Sakkendalen <Sakutynjala@gmail.com>
2020-12-09 09:12:04 +0200

    typo

--- src/styles/texts/infoText.js
+++ src/styles/texts/infoText.js
@@ -1,4 +1,4 @@
-const mytext = `I am a foruth year student in Tampere University of Applied Sciences. 
+const mytext = `I am a fourth year student in Tampere University of Applied Sciences. 
 Currently I am getting my degree on Software development. `;
 
 export default mytext ;
