3a2e9f9e58776ef505442be53f2fe3f2629919e9
Sakkendalen <Sakutynjala@gmail.com>
2020-12-11 15:46:40 +0200

    InfoText
    Scope:
    -InfoText.js
    
    Because:
    -Better writing

--- src/styles/texts/infoText.js
+++ src/styles/texts/infoText.js
@@ -1,7 +1,7 @@
-const mytext = `I am a fourth year student in Tampere University of Applied Sciences. 
-Currently I am getting my degree on Software development. I can work on projects both on-site and off-site.
-I'm looking for workplace where i can complete my trainee period and do my thesis. 
-I'm always eager to face new challenges what i can overcome with positive work enviroment.
+const mytext = `I'm a fourth year student in Tampere University of Applied Sciences. 
+Currently I'm finishing my degree on Software development. I can work on projects both on-site and off-site. 
+I'm looking for a workplace where I can complete my trainee period and produce my thesis. 
+I'm always eager to face and overcome new challenges in a positive work enviroment. 
 Feel free to contact me by email.`;
 
 export default mytext ;
